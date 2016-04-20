4bed63e869f29b520ac557868165c393e1d58407
Gideon I. Glass <gidglass@gmail.com>
2016-04-20 12:03:50 -0400

    initial commit

---
+++ database.py
@@ -0,0 +1,97 @@
+import pymysql
+
+class Database:
+  def __init__(self, host, username, password, db):
+    self._host = host
+    self._username = username
+    self._password = password
+    self._db = db
+
+  def _get_connection(self):
+    return pymysql.connect(
+      host = self._host,
+      user = self._username,
+      password = self._password,
+      db = self._db)
+
+  def _execute (self, sql):
+    db = self._get_connection()
+    cursor = db.cursor()
+    results = []
+    try:
+      cursor.execute(sql)
+      results = cursor.fetchall()
+      db.commit()
+    except:
+      db.rollback()
+    finally:
+      db.close()
+      return results
+
+  def create_table (self, schema, drop_if_exists=False):
+    TABLE = schema['table']
+    COLS, TYPES = self._split_fields(schema['fields'])
+
+    sql_table = "CREATE TABLE %s (" % TABLE
+    sql_schema = ", ".join('%s %s' % t for t in zip(COLS, TYPES)) + ")"
+
+    if drop_if_exists:
+      self._execute("DROP TABLE IF EXISTS %s" % table)
+
+    self._execute(sql_schematable + sql_schema)
+
+  def insert (self, row):
+    # sample_request_json = {
+    #   "row": {
+    #     "table": "EMPLOYEE",
+    #     "fields": {
+    #       "FIRST_NAME": "Gideon",
+    #       "LAST_NAME": "Glass",
+    #       "AGE": "24",
+    #       "SEX": "M",
+    #       "INCOME": "80000"
+    #     }
+    #   }
+    # }
+    TABLE = row['table']
+    COLS, VALS = self._split_fields(row['fields'])
+
+    # Construct query
+    sql_table = "INSERT INTO %s" % TABLE
+    sql_cols = "(%s)" % ", ".join(COLS)
+    sql_vals = "VALUES ('%s')" % "\', \'".join(VALS)
+    self._execute(sql_table + sql_cols + sql_vals)
+
+  def select (self, query):
+    # sample_query_json = {
+    #   "query": {
+    #     "table": "EMPLOYEE",
+    #     "cols": ["AGE", "INCOME"],
+    #     "fields": {
+    #       "FIRST_NAME": "Gideon",
+    #       "LAST_NAME": "Glass"
+    #     }
+    #   }
+    # }
+    TABLE, COLS, VALS = query['table'], query['cols'], query['fields']
+    sql_select = "SELECT %s " % ", ".join(COLS)
+    sql_from = "FROM %s.%s " % (self._db, TABLE)
+    sql_where = "WHERE %s;" % " AND ".join(self._json_to_where_clause(VALS))
+    return self._execute(sql_select + sql_from + sql_where)
+
+  ###
+  # HELPER METHODS
+  ##
+
+  def _json_to_where_clause (self, json):
+    result = []
+    for k,v in json.iteritems():
+        result.append(k + "=" + "\'" + v + "\'")
+    return result
+
+  def _split_fields(self, fields):
+    a, b = [], []
+    for k, v in fields.iteritems():
+      a.append(k)
+      b.append(v)
+    return (a, b)
---
+++ server.py
@@ -0,0 +1,90 @@
+from flask import Flask, request
+from database import Database
+import pymysql
+
+app = Flask(__name__)
+
+@app.route('/table', methods = ['POST'])
+def create_table ():
+  db = Database(
+    host = 'localhost', 
+    username = 'student', 
+    password = 'Password123!',
+    db = 'sample')
+
+  # sample_schema_json = {
+  #   "schema": {
+  #     "table": "EMPLOYEE",
+  #     "fields": {
+  #       "FIRST_NAME": "CHAR(20) NOT NULL",
+  #       "LAST_NAME": "CHAR(20)",
+  #       "AGE": "INT",
+  #       "SEX": "CHAR(1)",
+  #       "INCOME": "FLOAT"
+  #     }
+  #   }
+  # }
+  json = request.get_json()
+  db.create_table(json['schema'])
+
+  return "CREATED NEW TABLE: %s" % json['schema']['table']
+
+@app.route('/insert', methods = ['POST'])
+def insert_row ():
+  db = Database(
+    host = 'localhost', 
+    username = 'student', 
+    password = 'Password123!',
+    db = 'sample')
+
+  # sample_request_json = {
+  #   "row": {
+  #     "table": "EMPLOYEE",
+  #     "fields": {
+  #       "FIRST_NAME": "Gideon",
+  #       "LAST_NAME": "Glass",
+  #       "AGE": "24",
+  #       "SEX": "M",
+  #       "INCOME": "80000"
+  #     }
+  #   }
+  # }
+  json = request.get_json()
+
+  # TODO: ENCRYPT SENSATIVE JSON FIELDS HERE!!!
+  # e.g. json['row']['fields']['FIRST_NAME'] = encrypt(key, data)
+
+  db.insert(json['row'])
+
+  return "INSERTED NEW ROW: %s into %s" % (json['row']['fields'] ,json['row']['table'])
+
+@app.route('/query', methods = ['POST'])
+def query ():
+  db = Database(
+      host = 'localhost', 
+      username = 'student', 
+      password = 'Password123!',
+      db = 'sample')
+
+  # sample_query_json = {
+  #   "query": {
+  #     "table": "EMPLOYEE",
+  #     "cols": ["AGE", "INCOME"],
+  #     "fields": {
+  #       "FIRST_NAME": "Gideon",
+  #       "LAST_NAME": "Glass"
+  #     }
+  #   }
+  # }
+  json = request.get_json()
+
+  # TODO: DECRYPT SENSATIVE JSON FIELDS HERE!!!
+  # e.g. json['row']['fields']['FIRST_NAME'] = decrypt(key, data)
+
+  results = db.select(json['query'])  
+
+  # TODO: Parse results, for now just print...
+  return "RESULTS: " + str(results)
+
+if __name__ == '__main__':
+    app.run(debug=True)
